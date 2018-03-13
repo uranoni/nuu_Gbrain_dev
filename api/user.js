1abe058f6cd5b4df67c54533dbbe3f7ae5b4e1e2
KevinFan <kevin_ypfan@yahoo.com.tw>
2018-03-14 04:16:47 +0800

    fixd

--- api/user.js
+++ api/user.js
@@ -150,7 +150,8 @@ userRouter.get('/verifyMail/:token', (req, res) => {
         }
       })
     }
-    user.update({$set: { verify: true }})
+    return user.update({$set: { verify: true }})
+  }).then(() => {
     res.redirect('http://eecs.csie.nuu.edu.tw/login.html')
   }).catch((err) => {
     res.status(409).send(err)
