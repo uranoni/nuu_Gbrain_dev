447b7425be5ce653fb44745b43e1ec6abcd20f09
KevinFan <kevin_ypfan@yahoo.com.tw>
2018-04-11 20:38:24 +0800

    fixed xlsxCreate.js file

--- modules/xlsxCreate.js
+++ modules/xlsxCreate.js
@@ -9,17 +9,40 @@ const { Team } = require('../models/team');
 
 const allTeamXlsx = () => {
   const data = [
-    ['專題主題', '指導老師', '隊長', '組員', '備註']
+    ['專題主題', '指導老師', '指導老師email', '隊長', '隊長email', '檔案上傳進度', '組員一', '組員一email', '組員二', '組員二email', '組員三', '組員三email', '組員四', '組員四email', '組員五', '組員五email']
   ];
 
   Team.find().then((teams) => {
     teams.forEach(team => {
-      let registersName = team.registers.map(r => `${r.name}(${r.email})`);
-      const row = [ team.title, `${team.teacher.name}(${team.teacher.email})`, `${team.leader.name}(${team.leader.email})`, registersName.join(', ')]
+      const processCheck = ['register', 'plan', 'cover', 'video', 'warrant'];
+
+      let count = 0;
+      processCheck.forEach( p => {
+        if (team[p]) {
+          count ++
+        }
+      })
+      team.process = count / 5 * 100 + '%';
+
+      const row = [
+        team.title,
+        team.teacher.name,
+        team.teacher.email,
+        team.leader.name,
+        team.leader.email,
+        team.process
+      ]
+
+
+      for (let i = 0; i < team.registers.length; i++) {
+        row.push(team.registers[i].name);
+        row.push(team.registers[i].email);
+      }
+
       data.push(row);
     })
-    const buffer = xlsx.build([{ name: "工作表1", data: data}]);
-    const filePath = path.resolve('__dirname', '../public/form')
+    const buffer = xlsx.build([{ 'name': "工作表1", 'data': data}]);
+    const filePath = path.resolve(__dirname, '../public/form')
     if (!fs.existsSync(filePath)) {
       fs.mkdirSync('./public');
       fs.mkdirSync('./public/form');
